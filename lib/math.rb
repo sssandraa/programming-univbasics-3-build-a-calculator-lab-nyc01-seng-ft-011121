2020fcef9b0ed2fe2c1d0ab7dee7266097b4536d
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-23 16:48:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,3 +5,19 @@ return num1 + num2
 def subtraction(num1, num2)
   return num1 - num2
 end
+
+def multiplication(num1,num2)
+  return num1 * num2
+end
+
+def division(num1,num2)
+  return num1/num2
+end
+
+def modulo(num1,num2)
+  return num1 % num2
+end
+
+def square_root(num)
+return Math.sqrt(num)
+end
